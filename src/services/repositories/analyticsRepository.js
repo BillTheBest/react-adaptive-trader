e37a2be6a9797b6ba8c3c75e4c1229488e3df675
Ray Booysen <ray@sumatro.co.uk>
2015-07-31 10:33:49 +0100

    Turn on lint in the build and introduce and error to see it

--- src/services/repositories/analyticsRepository.js
+++ src/services/repositories/analyticsRepository.js
@@ -1,6 +1,5 @@
 import CurrencyPairPosition from 'model/analytics/currencyPairPosition.js';
 import HistoricPosition from 'model/analytics/historicPosition.js';
-import PositionUpdates from 'model/analytics/positionUpdates.js';
 
 export class AnalyticsRepository {
 
